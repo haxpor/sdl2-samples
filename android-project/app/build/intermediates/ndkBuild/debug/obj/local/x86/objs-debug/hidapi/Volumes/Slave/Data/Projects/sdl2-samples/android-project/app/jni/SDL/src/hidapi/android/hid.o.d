5b6a211137e45d60f4eb76ae20a432831eb1a329
Wasin Thonkaew <haxpor@gmail.com>
2018-10-19 07:13:57 +0800

    Fix android-project's flickering sample
    On android as tested, if not clear screen every frame then it will be flickering. Thus changed the orignal way the sameple'd want it to be.

--- android-project/app/build/intermediates/ndkBuild/debug/obj/local/x86/objs-debug/hidapi/Volumes/Slave/Data/Projects/sdl2-samples/android-project/app/jni/SDL/src/hidapi/android/hid.o.d
+++
@@ -1,5 +0,0 @@
-/Volumes/Slave/Data/Projects/sdl2-samples/android-project/app/build/intermediates/ndkBuild/debug/obj/local/x86/objs-debug/hidapi//Volumes/Slave/Data/Projects/sdl2-samples/android-project/app/jni/SDL/src/hidapi/android/hid.o: \
-  /Volumes/Slave/Data/Projects/sdl2-samples/android-project/app/jni/SDL/src/hidapi/android/hid.cpp \
-  /Volumes/Slave/Data/Projects/sdl2-samples/android-project/app/jni/SDL/src/hidapi/android/../hidapi/hidapi.h
-
-/Volumes/Slave/Data/Projects/sdl2-samples/android-project/app/jni/SDL/src/hidapi/android/../hidapi/hidapi.h:
--- android-project/app/jni/src/rectangles.c
+++ android-project/app/jni/src/rectangles.c
@@ -2,36 +2,58 @@
  *  rectangles.c
  *  written by Holmes Futrell
  *  use however you want
+ *
+ *  modified
+ *  as on Android (as tested on Huawei P9 Lite)
+ *  if not clear screen every frame, it's flickering
+ *  so now it will show one new rectangle for every 5 seconds,
+ *  before it clears and repeats the cycle.
 */
 
 #include "SDL.h"
 #include <time.h>
 #include "common.h"
 
+unsigned int lastTime = 0, currentTime;
+SDL_Rect drawingRect;
+SDL_Color color;
+
 void
 render(SDL_Renderer *renderer)
 {
-    Uint8 r, g, b;
-    int renderW;
-    int renderH;
+	/* Get current time since SDL library initialized */
+	currentTime = SDL_GetTicks();
+	/* Check if we should render randomly rectangle */
+	if (lastTime == 0 || currentTime > lastTime + 5000)
+	{
+		int renderW;
+		int renderH;
 
-    SDL_RenderGetLogicalSize(renderer, &renderW, &renderH);
+		SDL_RenderGetLogicalSize(renderer, &renderW, &renderH);
 
-    /*  Come up with a random rectangle */
-    SDL_Rect rect;
-    rect.w = randomInt(64, 128);
-    rect.h = randomInt(64, 128);
-    rect.x = randomInt(0, renderW);
-    rect.y = randomInt(0, renderH);
+		/*  Come up with a random rectangle */
+		drawingRect.w = randomInt(64, 128);
+		drawingRect.h = randomInt(64, 128);
+		drawingRect.x = randomInt(0, renderW);
+		drawingRect.y = randomInt(0, renderH);
 
-    /* Come up with a random color */
-    r = randomInt(50, 255);
-    g = randomInt(50, 255);
-    b = randomInt(50, 255);
+		/* Come up with a random color */
+		color.r = randomInt(50, 255);
+		color.g = randomInt(50, 255);
+		color.b = randomInt(50, 255);
 
-    /*  Fill the rectangle in the color */
-    SDL_SetRenderDrawColor(renderer, r, g, b, 255);
-    SDL_RenderFillRect(renderer, &rect);
+		/*  Fill the rectangle in the color */
+		SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, 255);
+		SDL_RenderFillRect(renderer, &drawingRect);
+
+		lastTime = currentTime;
+	}
+	// otherwise draw the previous rect
+	else
+	{
+		SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, 255);
+		SDL_RenderFillRect(renderer, &drawingRect);
+	}
 
     /* update screen */
     SDL_RenderPresent(renderer);
@@ -61,17 +83,13 @@ main(int argc, char *argv[])
     if (window == 0) {
         fatalError("Could not initialize Window");
     }
-    renderer = SDL_CreateRenderer(window, -1, 0);
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
     if (!renderer) {
         fatalError("Could not create renderer");
     }
 
     SDL_GetWindowSize(window, &windowW, &windowH);
     SDL_RenderSetLogicalSize(renderer, windowW, windowH);
-
-    /* Fill screen with black */
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-    SDL_RenderClear(renderer);
 
     /* Enter render loop, waiting for user to quit */
     done = 0;
@@ -81,8 +99,12 @@ main(int argc, char *argv[])
                 done = 1;
             }
         }
+
+		/* Fill screen with black */
+		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0xff);
+		SDL_RenderClear(renderer);
+
         render(renderer);
-        SDL_Delay(1);
     }
 
     /* shutdown SDL */
